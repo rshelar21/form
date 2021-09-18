c34c3977c4f9c63dd6b79e1d2e764ac18f89ae69
rohan <rshelar796@gmail.com>
2021-09-18 21:21:21 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 const mongoose = require('mongoose');
